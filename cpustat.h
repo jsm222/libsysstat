e51647df17ce46b5e34e21acfe34d6f4717382ba
Jesper Schmitz Mouridsen <jesper@schmitz.computer>
2019-06-15 23:10:05 +0200

    FreeBSD patches from ports applied.

--- cpustat.h
+++ cpustat.h
@@ -27,14 +27,20 @@
 #ifndef LIBSYSSTAT__CPU_STAT__INCLUDED
 #define LIBSYSSTAT__CPU_STAT__INCLUDED
 
-
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 #include <QtCore/QObject>
 
 #include "basestat.h"
 
 
 namespace SysStat {
-
+#ifdef HAVE_SYSCTL_H
+    char *GetFirstFragment(char *string, const char *delim);
+    int GetCpu(void);
+    ulong CurrentFreq(void);
+#endif
 class CpuStatPrivate;
 
 class SYSSTATSHARED_EXPORT CpuStat : public BaseStat
